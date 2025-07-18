a1fff62ec815c2e346572a77cfc93827a5ab06d9
hoodiedan <obodedaniel3@gmail.com>
2025-07-18 04:27:40 +0100

    feat: update about page content and styles

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,223 @@
 <script setup lang="ts">
-import ContainerWrapper from '~/src/components/container-wrapper.vue';
-
+import ContainerWrapper from "~/src/components/container-wrapper.vue";
 </script>
 
 <template>
-    <div class="py-5 flex flex-col gap-10">
-        <ContainerWrapper>
-            <div class="flex">
-                div
-            </div>
-        </ContainerWrapper>
-    </div>
+	<div class="py-10 flex flex-col gap-10">
+		<ContainerWrapper>
+			<div class="flex flex-col-reverse md:flex-row">
+				<div class="w-full md:w-5/12 p-5">
+					<h2 class="!font-bold mb-5">
+						Who we
+						<span class="!font-normal !font-zenith italic"
+							>are</span
+						>
+					</h2>
+					<p>
+						Kingsville Church was commissioned on a beautiful Easter
+						Sunday in 2009. With worshippers from all the senatorial
+						districts of Lagos state, we are a cosmopolitan church
+						with an uncompromising tradition of life in the Word and
+						the Spirit. We are driven by a desire for
+						self-actualization in God; where every worshipper has
+						unforgettable encounters and experiences with the word
+						of His grace.
+					</p>
+
+					<div class="mt-6">
+						<div class="mt-2">
+							<h5 class="!font-zenith !font-normal mb-4">
+								Service Times
+							</h5>
+							<div>
+								<h6 class="font-dark-grey mb-3">Sundays</h6>
+								<p>
+									English Language Service at 9:00am to
+									11:30am
+								</p>
+								<p>
+									Yoruba Language Service at 11:30am to 1:15pm
+								</p>
+							</div>
+							<div class="mt-4">
+								<h6 class="font-dark-grey mb-3">
+									Wednesdays (Online)
+								</h6>
+								<p>Every week from 6:30pm to 8:00pm on Zoom</p>
+							</div>
+						</div>
+					</div>
+					<div class="mt-6">
+						<div class="mt-2">
+							<h5 class="!font-zenith !font-normal mb-4">
+								Locations
+							</h5>
+							<p class="mb-1">
+								2 Akinwunmi street, off Hughes Ave,
+								Alagomeji-Yaba, Lagos
+							</p>
+							<p>101 Okota Rd, Ilasamaja, Lagos 102214, Lagos.</p>
+						</div>
+					</div>
+				</div>
+				<div
+					class="w-full md:w-7/12 h-[35rem] bg-placeholder rounded-md"
+				></div>
+			</div>
+		</ContainerWrapper>
+
+		<ContainerWrapper class="mt-24">
+			<div class="flex flex-col md:flex-row md:items-center">
+				<div
+					class="w-full md:w-1/3 h-72 bg-placeholder rounded-md"
+				></div>
+				<div class="w-full md:w-2/3 p-10">
+					<h5 class="!font-zenith text-dark-grey mb-5">
+						Our Passion
+					</h5>
+					<h4 class="my-7 !font-bold">The future we're building</h4>
+					<p>
+						4 Pillars of Kingsville Church: We are passionate about
+						<span class="!font-zenith italic"
+							>‘…raising successful godly lights’</span
+						>
+						in their:
+					</p>
+					<ul class="mt-4">
+						<li>Career</li>
+						<li>Relationship</li>
+						<li>Health</li>
+						<li>Finance</li>
+					</ul>
+				</div>
+			</div>
+		</ContainerWrapper>
+
+		<ContainerWrapper class="mt-24">
+			<div class="flex flex-col-reverse md:flex-row md:items-center">
+				<div class="w-full md:w-2/3 p-10">
+					<h5 class="!font-zenith text-dark-grey mb-5">Our Vision</h5>
+					<h4 class="my-7 !font-bold">Our Daily Pursuit</h4>
+					<p>
+						At Kingsville Church, our vision is clear: to raise
+						successful godly lights who shine in every area of life.
+						This vision fuels all we do — from worship and
+						discipleship to outreach and leadership.
+					</p>
+
+					<h6 class="mt-4">Our church is:</h6>
+					<ul class="mt-2">
+						<li>A soul-winning church</li>
+						<li>A word-of-faith church</li>
+						<li>A spirit-filled church</li>
+						<li>
+							Making a difference as a lighthouse through the love
+							of God
+						</li>
+						<li>
+							Equipping our members to be successful in life and
+							ministry
+						</li>
+						<li>
+							Equipping for godliness and preparing our people for
+							eternity.
+						</li>
+					</ul>
+				</div>
+				<div
+					class="w-full md:w-1/3 h-72 bg-placeholder rounded-md"
+				></div>
+			</div>
+		</ContainerWrapper>
+
+		<ContainerWrapper class="mt-24">
+			<div class="flex flex-col md:flex-row md:items-center">
+				<div
+					class="w-full md:w-1/3 h-72 bg-placeholder rounded-md"
+				></div>
+				<div class="w-full md:w-2/3 p-10">
+					<h5 class="!font-zenith text-dark-grey mb-5">
+						Our Core Values
+					</h5>
+					<h4 class="my-7 !font-bold">Living the Kingsville way</h4>
+					<p>
+						Our values define our culture. SEAGRIP is more than an
+						acronym — it’s how we serve, grow, and reflect Christ
+						daily. These principles keep us grounded in purpose and
+						unified in spirit.
+					</p>
+					<ul class="mt-4">
+						<li>
+							<span class="!font-zenith italic">S</span
+							>pirituality
+						</li>
+						<li>
+							<span class="!font-zenith italic">E</span>xcellence
+						</li>
+						<li>
+							<span class="!font-zenith italic">A</span
+							>ccountability
+						</li>
+						<li><span class="!font-zenith italic">G</span>rowth</li>
+						<li>
+							<span class="!font-zenith italic">R</span
+							>esponsibility
+						</li>
+						<li>
+							<span class="!font-zenith italic">I</span>ntegrity
+						</li>
+						<li>
+							<span class="!font-zenith italic">P</span
+							>eople-friendliness
+						</li>
+					</ul>
+				</div>
+			</div>
+		</ContainerWrapper>
+
+		<ContainerWrapper>
+			<div class="flex flex-col-reverse md:flex-row">
+				<div class="w-full md:w-7/12 p-5">
+					<h2 class="!font-bold mb-5">
+						Meet
+						<span class="!font-normal !font-zenith italic"
+							>Reverend Paul Rotua</span
+						>
+					</h2>
+					<p>
+						A pastor from the heart, Reverend Paul Rotua has been in
+						the Lord’s service since his teenage years as president
+						of the Christian Fellowship of the Federal Government
+						College, Ijanikin, Lagos; and later as an associate
+						pastor in Victory Christian Fellowship (VCF) at the
+						University of Ibadan, where he studied Human Nutrition.
+						Fondly called Pastor Paul, his life has been
+						characterised by the manifestation of the grace of God;
+						which has been the main thrust of his service to the
+						Body of Christ and the world.
+					</p>
+					<p class="mt-3">
+						With a passion to see people attain self-actualization
+						by manifesting their potentials in the grace and love of
+						God, he shares the message of God’s awesome grace with
+						simplicity, clarity and an unwavering sincerity to see
+						people’s lives transformed. A regular speaker at
+						conferences at home and abroad, he is the host of the
+						highly acclaimed Encounters with Grace on radio and
+						Encounters with Grace Live, a specialised teaching
+						outreach.
+					</p>
+					<p class="mt-3">
+						Happily married to his beloved Olusade who co-pastors
+						with him and is also the convener of the annual
+						Extraordinary Women Conference. They are blessed with
+						three lovely daughters, Melissa, Melody and Megan.
+					</p>
+				</div>
+				<div
+					class="w-full md:w-5/12 h-[35rem] bg-placeholder rounded-md"
+				></div>
+			</div>
+		</ContainerWrapper>
+	</div>
 </template>
